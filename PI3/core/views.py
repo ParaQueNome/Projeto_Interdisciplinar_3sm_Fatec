97da8e343f382ad64cf16e35ddc35d4f34e3128c
ParaQueNome <guiaugusto455@gmail.com>
2023-10-26 15:44:27 -0300

    Alterando documentação das bibliotecas

--- PI3/core/views.py
+++ PI3/core/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from .models import Empresas
 
 def cadastro(request):
+    empresas = Empresas(nome ='JAVA')
+    empresas.save()
     return render(request, 'cadastro.php')
-    empresa = Empresas.save( )
+    
