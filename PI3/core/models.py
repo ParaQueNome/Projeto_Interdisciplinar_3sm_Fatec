1b4420b9de4660cb3cdc762dc70720365a537baa
ParaQueNome <guiaugusto455@gmail.com>
2023-10-25 23:05:50 -0300

    Testando o banco de dados

--- PI3/core/models.py
+++ PI3/core/models.py
@@ -1,4 +1,9 @@
-from djongo import models
+from django.db import models
 
 
 # Create your models here.
+class Empresas(models.Model):
+    nome = models.CharField(max_length=100, help_text='Nome da Empresa')
+
+    def __str__(self):
+        return self.nome
--- PI3/core/views.py
+++ PI3/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Empresas
 
 def cadastro(request):
     return render(request, 'cadastro.php')
+    empresa = Empresas.save( )
