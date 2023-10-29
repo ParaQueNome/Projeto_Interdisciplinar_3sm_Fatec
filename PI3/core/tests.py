fca685cd2784ce497a3f83429cb0a8be8ac7b91c
ParaQueNome <guiaugusto455@gmail.com>
2023-10-29 00:21:02 -0300

    Adicionando testes

--- PI3/core/tests.py
+++ PI3/core/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
+from django.test import RequestFactory
+from .views import cadastro
+
 # Create your tests here.
+
+class cadastroTest(TestCase):
+    def test_cadastro(self):
+        
+        request = RequestFactory().get('/cadastro')
+        response = cadastro(request)
+        self.assertEqual(response.status_code, 200)
+    def test_string(self):
+
+        self.assertGreater(self.len(),5)
+
+
+
