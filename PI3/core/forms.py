c1e74b2773d6aad8863a1c8a8a49e462e324fb39
ParaQueNome <guiaugusto455@gmail.com>
2023-10-30 17:42:44 -0300

    Adicionando restante do cadastro de empresas

--- PI3/core/forms.py
+++ PI3/core/forms.py
@@ -1,11 +1,13 @@
+import re
 from django import forms
 
 class EmpresaForm(forms.Form):
     nome = forms.CharField(max_length=50, required= True)
     email = forms.CharField(max_length=50, required= True)
     cnpj = forms.CharField(max_length=18, widget=forms.TextInput(attrs={'placeholder': '99.999.999/9999-99'}))
-    ##cep = forms.IntegerField(required= True)
-    ##numero = forms.IntegerField(required= True)
+    cep = forms.CharField(max_length=9, widget=forms.TextInput(attrs={'placeholder': '99.999-999', 'id': 'cep'}))
+    numero = forms.IntegerField(required= True)
+    senha = forms.CharField(max_length=50, required= True, widget= forms.TextInput(attrs={'type' : 'password'}))
     ramo = forms.CharField(max_length=50, required= False)
     descricao = forms.CharField(max_length=50, required= False)
     telefone = forms.IntegerField(required= False)
@@ -32,3 +34,37 @@ class EmpresaForm(forms.Form):
             raise forms.ValidationError('CNPJ invalido')
             
         return cnpj
+    def cleanCep(self):
+        cep = self.cleaned_data['cep']
+        if len(cep) < 8:
+            raise forms.ValidationError('CEP invalido')
+            
+        return cep
+    
+    def cleanNumero(self):
+        numero = self.cleaned_data['numero']
+        if numero < 1:
+            raise forms.ValidationError('Numero invalido')
+            
+        return numero
+    def cleanSenha(self):
+        senha = self.cleaned_data['senha']
+        if len(senha) < 8:
+            
+            if senha:
+            # Verifique se a senha contém pelo menos uma letra maiúscula
+                if not re.search(r'[A-Z]', senha):
+                    raise forms.ValidationError('A senha deve conter pelo menos uma letra maiúscula.')
+
+            # Verifique se a senha contém pelo menos uma letra minúscula
+                if not re.search(r'[a-z]', senha):
+                    raise forms.ValidationError('A senha deve conter pelo menos uma letra minúscula.')
+
+                 # Verifique se a senha contém pelo menos um número
+                if not re.search(r'\d', senha):
+                    raise forms.ValidationError('A senha deve conter pelo menos um número.')
+
+                # Verifique se a senha contém pelo menos um caractere especial
+                if not re.search(r'[!@#$%^&*()_+{}":;\'<>,.\\-]', senha):
+                    raise forms.ValidationError('A senha deve conter pelo menos um caractere especial.')
+        return senha
--- PI3/core/views.py
+++ PI3/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django import forms
 from .forms import EmpresaForm
 from .services .ConexaoService import ConexaoService
@@ -15,17 +15,21 @@ def cadastro(request):
                 nome = form.clean_nome()
                 email = form.clean_email()
                 cnpj = form.clean_cnpj()
+                cep = form.cleanCep()
+                numero = form.cleanNumero()
+                senha = form.cleanSenha()
             
-            except Exception as e:
-                return HttpResponse(f'Erro: {str(e)}')
+            except forms.ValidationError as e:
+                return render(request, 'cadastro.html', {'form': form})
+            
 
             
             conexao = ConexaoService()
             client = MongoConnection(conexao)
-            client.insert(nome = nome, email = email,cnpj = cnpj)
+            client.insert(nome = nome, email = email,cnpj = cnpj, cep=cep,numero = numero, password = senha)
             
             return render(request, 'cadastro.html',{'form':EmpresaForm()})
-
+        
     else:
         return render(request, 'cadastro.html',{'form':EmpresaForm()})
     
